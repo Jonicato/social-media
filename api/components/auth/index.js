cff1cd221974a9d78deb050eddc878f7dee24311
Jonicato <iam.jonicato@gmail.com>
2023-11-07 13:47:19 -0600

    validacion remote db

--- api/components/auth/index.js
+++ api/components/auth/index.js
@@ -1,5 +1,10 @@
-const store = require('../../../store/remote-mysql');
-
+const config = require('../../../config');
+let store;
+if(config.remoteDB == true) {
+    store = require('../../../store/remote-mysql');
+} else {
+    const store = require('../../../store/mysql');
+}
 const controller = require('./controller');
 
 module.exports = controller(store);
